d98ab5f56b5365bb6a5bbea94cfab410185fa555
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-12-12 20:27:46 +0200

    Changed: Added the delete on search functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('#searchForm');
+const btn = document.querySelector('.search-btn')
 
 document.addEventListener('DOMContentLoaded', async function () {
     console.log('Hello')
@@ -11,19 +12,34 @@ document.addEventListener('DOMContentLoaded', async function () {
 form.addEventListener('click', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
-    const config = {params: {q: searchTerm}};
+    const config = { params: { q: searchTerm } };
     const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+
+
     makeImages(res.data)
 
     form.elements.query.value = '';
+
+
 })
 
+btn.addEventListener('click', function (e) {
+    var elements = document.querySelectorAll('.movie_image');
+    elements.forEach(function (element) {
+        element.remove();
+    });
+
+})
+
+
+
 const makeImages = (shows) => {
-    for ( let result of shows ) {
-        if (result.show.image){
+    for (let result of shows) {
+        if (result.show.image) {
             const img = document.createElement('IMG');
             img.src = result.show.image.medium;
+            img.classList.add('movie_image')
             document.body.append(img);
-        } 
+        }
     }
 }
