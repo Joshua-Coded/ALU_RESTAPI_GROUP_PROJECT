1ecec4348acd76c6ede65fb38c7323191ade9513
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-12-12 17:24:48 +0200

    Changed: Added a functionality to populate the UI with movies when the browser loads

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const form = document.querySelector('#searchForm');
+
+document.addEventListener('DOMContentLoaded', async function () {
+    console.log('Hello')
+    const config = { params: { q: "heist" } };
+    console.log(config)
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data)
+}, false);
+
 form.addEventListener('click', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
     const config = {params: {q: searchTerm}};
-   const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
     makeImages(res.data)
 
     form.elements.query.value = '';
@@ -13,9 +22,8 @@ const makeImages = (shows) => {
     for ( let result of shows ) {
         if (result.show.image){
             const img = document.createElement('IMG');
-    img.src = result.show.image.medium;
-    document.body.append(img);
-        }
-        
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        } 
     }
 }
